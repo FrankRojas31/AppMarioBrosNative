74f9fbc1c095e805446c7ef4373280a49f405c01
Frank Rojas <fco3190@gmail.com>
2024-01-30 15:46:36 -0500

    Se quita un hook que esta sin utilizar para clean code.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Text, View, TouchableOpacity, Image, FlatList, Modal } from "react-native";
 import tw, { useAppColorScheme } from "twrnc";
 
